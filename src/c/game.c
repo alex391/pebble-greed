7873d11cfb523cc9521afb76bc7d4bee14f3bb4d
Alex Leute <alex@alexleute.com>
2026-06-05 02:18:47 -0400

    (trying to get .gitignore to stick)

---
+++ src/c/game.c
@@ -0,0 +1,370 @@
+/*
+Copyright (c) 2024 Alex Leute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+  This file contains all of the actual game logic - so everythig that isn't
+  drawing to the screen or getting button inputs
+
+  This has been translated from an Aurduino C++ version for Wio Terminal
+  https://gist.github.com/alex391/c13f53c876c2ca99fafaacd4404522a4
+*/
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+// two bigger than the actuall playable area to pad with zero around the edges
+#define BOARD_WIDTH (19 + 2)
+#define BOARD_HEIGHT (12 + 2)
+
+// In milliseconds
+#define BLINKING_DELAY 500
+
+#define DEBUG false
+
+void fillBoard();
+void drawBoard();
+void heartbeat();
+void blinkCursor();
+void movement();
+struct MovementVector getButtons();
+void drawCursor(uint32_t color);
+size_t getValidDirections(struct MovementVector *directions);  // directions should have enough space for 8 MovementVectors
+bool checkDirection(struct MovementVector direction); // true if the player won't hit any zeros going that way
+bool movementVectorIn(struct MovementVector needle, struct MovementVector *haystack, size_t size);
+void drawDirection(struct MovementVector direction);
+void hint();
+void reset();
+bool isEmpty(struct MovementVector this);
+uint8_t movementDistance(struct MovementVector this);
+bool MovementVectorEquals();
+
+uint32_t score = 0;
+
+uint8_t board[BOARD_HEIGHT][BOARD_WIDTH] = { 0 };
+
+struct Player {
+  uint8_t x;
+  uint8_t y;
+};
+
+struct Player player = { 1, 1 };
+
+struct MovementVector {
+  int8_t x;
+  int8_t y;
+
+};
+
+bool isEmpty(struct MovementVector this) {
+  return this.x == 0 && this.y == 0;
+}
+
+uint8_t movementDistance(struct MovementVector this) {
+ return board[player.y + this.y][player.x + this.x];
+}
+
+bool MovementVectorEquals(struct MovementVector lhs, struct MovementVector rhs) {
+  return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+void setup() {
+  randomSeed(analogRead(0));  // Generate some random noise
+
+  reset();
+}
+
+void loop() {
+  heartbeat();
+  hint();
+  if (!digitalRead(WIO_KEY_A)) {
+    demo = true;
+  }
+  movement();
+  blinkCursor();
+  delay(POLLING_DELAY);
+  clock += POLLING_DELAY;
+}
+
+void reset() {
+  score = 0;
+  demo_score = 0;
+  demo = false;
+  player.x = random(1, BOARD_WIDTH - 1);
+  player.y = random(1, BOARD_HEIGHT - 1);
+
+  fillBoard();
+  drawBoard();
+}
+
+void fillBoard() {
+  for (size_t y = 1; y < BOARD_HEIGHT - 1; y++) {
+    for (size_t x = 1; x < BOARD_WIDTH - 1; x++) {
+      board[y][x] = random(1, 8);
+    }
+  }
+  board[player.y][player.x] = 0;
+}
+
+uint32_t lightState = HIGH;
+// blink the light so you know it's working
+void heartbeat() {
+  digitalWrite(LED_BUILTIN, lightState);
+  lightState = !lightState;
+}
+
+void drawBoard() {
+  background.fillSprite(TFT_BLACK);
+  const uint32_t colors[8] = {
+    // Colors to pick from
+    TFT_BLACK,
+    TFT_WHITE,
+    TFT_LIGHTGREY,
+    TFT_GREEN,
+    TFT_GREENYELLOW,
+    TFT_YELLOW,
+    TFT_ORANGE,
+    TFT_RED,
+  };
+  for (size_t y = 1; y < BOARD_HEIGHT - 1; y++) {
+    for (size_t x = 1; x < BOARD_WIDTH - 1; x++) {
+      background.drawChar((x - 1) * FONT_WIDTH, (y - 1) * FONT_HEIGHT, '0' + board[y][x], colors[board[y][x]], TFT_BLACK, FONT_SIZE);
+    }
+  }
+  background.pushSprite(0, 0);
+}
+
+void blinkCursor() {
+  if ((clock % (BLINKING_DELAY * 2)) < BLINKING_DELAY) {
+    drawCursor(TFT_WHITE);
+  } else {
+    drawCursor(TFT_BLACK);
+  }
+}
+
+void drawCursor(uint32_t color) {
+  cursor.fillSprite(color);
+  cursor.pushSprite((player.x - 1) * FONT_WIDTH, (player.y - 1) * FONT_HEIGHT);
+}
+
+#if DEBUG
+size_t previous_directions_count = 9;
+#endif
+
+void movement() {
+  MovementVector valid_directions[8] = { 0 };
+  size_t valid_directions_count = getValidDirections(valid_directions);
+#if DEBUG
+  if (valid_directions_count != previous_directions_count) {
+    Serial.printf("valid_directions_count: %d\n", valid_directions_count);
+    previous_directions_count = valid_directions_count;
+  }
+#endif
+  if (valid_directions_count == 0) {
+    // Start the game over with the demo player
+    char game_over_buff[28] = { 0 };  // big enough for "Game over! Score: 999 99.9%\0"
+    float percentage = (float)score * 100.0f / (float)((BOARD_HEIGHT - 2) * (BOARD_WIDTH - 2));
+    snprintf(game_over_buff, sizeof(game_over_buff), "Game over! Score: %d %.1f%%", score, percentage);
+    tft.drawString(game_over_buff, 0, 0);
+    while (getButtons().isEmpty())
+      ;
+    reset();
+  }
+  MovementVector buttons;
+  if (demo) {
+    buttons = greedyButtons(valid_directions, valid_directions_count);
+  } else {
+    buttons = getButtons();
+  }
+  if (buttons.isEmpty()) {
+    return;
+  }
+  MovementVector held_buttons = { 0 };
+  bool redraw = true;
+  do {
+    // have to do some of the main loop things here
+    heartbeat();
+    blinkCursor();
+    held_buttons = getButtons();
+    // make it easier to go diagonally.
+    if (buttons.x == 0 && held_buttons.x != 0) {
+      buttons.x = held_buttons.x;
+      redraw = true;
+    }
+    if (buttons.y == 0 && held_buttons.y != 0) {
+      buttons.y = held_buttons.y;
+      redraw = true;
+    }
+#if DEBUG
+    Serial.printf("buttons.x: %d, buttons.y: %d\n", buttons.x, buttons.y);
+#endif
+    if (redraw && movementVectorIn(buttons, valid_directions, valid_directions_count)) {
+      drawBoard();
+      drawDirection(buttons);
+      background.pushSprite(0, 0);
+      redraw = false;
+    }
+    delay(POLLING_DELAY);
+    clock += POLLING_DELAY;
+  } while (!(held_buttons.isEmpty()));  // wait for release
+
+
+  if (!movementVectorIn(buttons, valid_directions, valid_directions_count)) {
+    drawBoard();  // Sometimes need to clear the board after an invalid direction (if the direction was previously valid the hint will still be there)
+    return;
+  }
+
+  uint8_t movement_distance = buttons.movementDistance();
+  for (; movement_distance > 0; movement_distance--) {
+    player.x += buttons.x;
+    player.y += buttons.y;
+    board[player.y][player.x] = 0;
+    score++;
+  }
+#if DEBUG
+  Serial.printf("player.x = %d, player.y = %d\n", player.x, player.y);
+#endif
+  board[player.y][player.x] = 0;
+  drawBoard();
+}
+
+MovementVector getButtons() {
+  MovementVector v = { 0 };
+  if (!digitalRead(WIO_5S_UP)) {
+    v.y += -1;
+  }
+  if (!digitalRead(WIO_5S_RIGHT)) {
+    v.x += 1;
+  }
+  if (!digitalRead(WIO_5S_DOWN)) {
+    v.y += 1;  // Down is positive y
+  }
+  if (!digitalRead(WIO_5S_LEFT)) {
+    v.x += -1;
+  }
+  return v;
+}
+
+// Play greed... greedily!
+MovementVector greedyButtons(MovementVector *directions, size_t size) {
+  uint8_t max{ 0 };
+  MovementVector max_direction{};
+  for (size_t i; i < size; i++) {
+    if (directions[i].movementDistance() > max) {
+      max = directions[i].movementDistance();
+      max_direction = directions[i];
+    }
+  }
+  return max_direction;
+}
+
+
+// Leaving it up to the caller to make sure directions enough space for 8 directions in it
+// returns the number of directions that are valid
+size_t getValidDirections(MovementVector *directions) {
+  size_t directions_index = 0;
+  for (int8_t x = -1; x <= 1; x++) {
+    for (int8_t y = -1; y <= 1; y++) {
+      if (x == 0 && y == 0) {
+        continue;
+      }
+      MovementVector v = { x, y };
+      if (checkDirection(v)) {
+        directions[directions_index++] = v;
+      }
+    }
+  }
+  return directions_index;
+}
+
+bool checkDirection(MovementVector direction) {
+  uint8_t movement_distance = direction.movementDistance();
+  if (movement_distance == 0) {
+    return false;
+  }
+  Player temp_player = player;
+  for (; movement_distance > 0; movement_distance--) {
+    temp_player.x += direction.x;
+    temp_player.y += direction.y;
+    if (board[temp_player.y][temp_player.x] == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void drawDirection(MovementVector direction) {
+#if DEBUG
+  Serial.println("Drawing a direction!");
+#endif
+  uint8_t movement_distance = direction.movementDistance();
+  if (movement_distance == 0) {
+    return;  // shouldn't be here, but not so bad
+  }
+  Player temp_player = player;
+  movement_distance++;  // Skip the first one
+  for (; movement_distance > 1 /* 1 because we skipped the first one */; movement_distance--) {
+    temp_player.x += direction.x;
+    temp_player.y += direction.y;
+    if (board[temp_player.y][temp_player.x] == 0) {
+#if DEBUG
+      Serial.println("drawDirection should only be called with valid directions!");
+#endif
+      return;  // HEY! Why are we here! Stop it!
+    }
+#if DEBUG
+    Serial.printf("Draw a rectangle at %d, %d\n", temp_player.x, temp_player.y);
+#endif
+    background.fillRect((temp_player.x - 1) * FONT_WIDTH, (temp_player.y - 1) * FONT_HEIGHT, FONT_WIDTH, FONT_HEIGHT, TFT_NAVY);
+  }
+}
+
+bool movementVectorIn(MovementVector needle, MovementVector *haystack, size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    if (needle == haystack[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void hint() {
+  if (digitalRead(WIO_KEY_C)) {
+    return;
+  }
+  MovementVector valid_directions[8]{ 0 };
+  size_t valid_directions_count = getValidDirections(valid_directions);
+  for (size_t i = 0; i < valid_directions_count; i++) {
+    drawDirection(valid_directions[i]);
+  }
+#if DEBUG
+  Serial.println("Pushing a sprite");
+#endif
+  background.pushSprite(0, 0);
+  while (!digitalRead(WIO_KEY_C) && getButtons().isEmpty()) {
+    heartbeat();
+    blinkCursor();
+    delay(POLLING_DELAY);
+    clock += POLLING_DELAY;
+  }  // Wait for release
+  drawBoard();
+}
